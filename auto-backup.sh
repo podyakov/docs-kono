a8219b90d9a7b574dcafd1fa42e5ae0d61c05d86
Alexander Podyakov <a.e.podyakov@mail.ru>
2026-04-09 16:58:48 +0300

    add auto-backup.sh

---
+++ auto-backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /Users/eka/Desktop/docs-kono
+
+git add .
+
+# коммит только если есть изменения
+if ! git diff --cached --quiet; then
+  git commit -m "auto backup $(date '+%Y-%m-%d %H:%M:%S')"
+  git push origin main
+fi
+
+# добавить этот файл в крон
+# crontab -e
+# */5 * * * * /Users/eka/Desktop/docs-kono/auto-backup.sh
